007d1e3a34bb82efc691eae4b9fc9563acff9398
hussainjafri786 <164414058+hussainjafri786@users.noreply.github.com>
2024-03-21 13:34:41 -0700

    my first project

---
+++ huss.js
@@ -0,0 +1,22 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter the First Number:", type: "number", name: "firstNumber" },
+    { message: "Enter the Second Number:", type: "number", name: "secondNumber" },
+    { message: "please select the operator you would like to perform operation", type: "list", name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"] },
+]);
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("please select a valid operator");
+}
---
+++ huss.ts
@@ -0,0 +1,26 @@
+#! usr/bin/env
+
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+{message: "Enter the First Number:",type:"number",name:"firstNumber"},
+{message: "Enter the Second Number:",type:"number",name:"secondNumber"},
+{message: " select the operator you would like to perform operation",type:"list",name:"operator",
+choices:["Addition","Subtraction","Multiplication","Division"]},
+]);
+
+if(answer.operator === "Addition"){
+    console.log(answer.firstNumber + answer.secondNumber)
+}
+else if (answer.operator ==="Subtraction"){
+    console.log(answer.firstNumber - answer.secondNumber)
+}
+else if (answer.operator ==="Multiplication"){
+    console.log(answer.firstNumber * answer.secondNumber)
+}
+else if (answer.operator ==="Division"){
+    console.log(answer.firstNumber / answer.secondNumber)
+}
+else{
+    console.log("please select a valid operator")
+}
+
